c313626e344bbf66a359eb41abe8a5ecdb162e8c
Julia Shub <Julia.Shub@safe-t.com>
2019-02-18 22:51:06 +0200

    bla

--- home-assignments/session1/exercise1.py
+++ home-assignments/session1/exercise1.py
@@ -9,11 +9,6 @@ The JSON will be in the following format (see below)
  the hash will be of pairs of names and ages (name will be the key and age will be the value)
  the array will be a list of ages
 
- note for Yaron - this excercise needs parsing to understand.
- The array is already contained withing the sample file, but the exercise is written as if it needs to be created.
- Also, this seems a bit much for people who had not touched python earliet than 2 weeks ago. it requires better wtitten
- questions, broken down to smaller pieces, or more time to do the homeworks since this is a lot of research for people who don't neccessarily know what they are looking for
-
 Usage:
 	python exercise1.py
 """
@@ -25,7 +20,7 @@ from pprint import pprint as pp
 
 
 # Read an input parameter with a JSON file name
-# ppl_ages_dict C:\Users\julia.shub\PycharmProjects\OpsSchool\Session1-HW\opsschool3-coding\home-assignments\session1\my_list.json
+# C:\Users\julia.shub\PycharmProjects\OpsSchool\Session1-HW\opsschool3-coding\home-assignments\session1\my_list.json
 def get_json():
     file_name = os.path.join(input('JSON full path: '))
     with open(file_name, encoding='UTF-8') as f:
@@ -34,11 +29,7 @@ def get_json():
 
 
 def parse_json(json_data):
-    '''the program will go over the list of people and divide them to buckets based on their ages
-    each bucket will hold all the names of the people with age between the partition key and the following partition key
-    bucket  ‘20-25’ will hold a list of name that their age is between 20 and 25 (not including)
-    this data will them be saved in a yaml format in a file with the same name as the input file with changed extension.
-    bonus - if there is someone that doesn't fall into any of the baskets it will create a new bucket based on the oldest person and add all the people that fall into this bucket
+    '''the program will go over the list of people and divide them to buckets based on their ages + bonus bucket for people that are not in the range defined by the array in the json file
 
     Args:
          json_data: json_data is a dictionary imported from a file
@@ -92,7 +83,7 @@ def parse_json(json_data):
     with codecs.open("my_list.yaml", "w", "utf-8") as write_file:
         for age_range in ranges:
             yaml.dump({"{0}-{1}".format(age_range['Start'], age_range['End']): age_range['People']}, write_file)
-        yaml.dump({'rest_of_people': rest_of_ppl}, write_file)
+        yaml.dump({'rest_of_people': rest_of_ppl}, write_file, encoding=('utf-8'))
 
 
 def main():
